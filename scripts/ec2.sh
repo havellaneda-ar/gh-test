46c8433c12f47aba9a5be51ba99b977b814172c6
Hernan <47876073+havellaneda-ar@users.noreply.github.com>
2022-10-21 18:13:43 -0300

    Update ec2.sh

--- scripts/ec2.sh
+++ scripts/ec2.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 cat <<EOF >/home/ubuntu/user-data.sh
 #!/bin/bash
-wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
-chmod +x ./jq
-sudo cp jq /usr/bin
-curl --request POST 'https://api.github.com/repos/havellaneda-ar/gh-test/actions/runners/registration-token' --header "Authorization: token ${PERSONAL_ACCESS_TOKEN}" > output.txt
-runner_token=\$(jq -r '.token' output.txt)
-mkdir ~/actions-runner
-cd ~/actions-runner
-curl -O -L https://github.com/actions/runner/releases/download/v2.263.0/actions-runner-linux-x64-2.263.0.tar.gz
-tar xzf ~/actions-runner/actions-runner-linux-x64-2.263.0.tar.gz
-rm ~/actions-runner/actions-runner-linux-x64-2.263.0.tar.gz
-~/actions-runner/config.sh --url https://github.com/havellaneda-ar/gh-test/ --token \$runner_token --name "Github EC2 Runner" --unattended
-~/actions-runner/run.sh
+# Create a folder
+mkdir actions-runner && cd actions-runner# Download the latest runner package
+curl -o actions-runner-linux-x64-2.298.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.298.2/actions-runner-linux-x64-2.298.2.tar.gz
+# Optional: Validate the hash
+echo "0bfd792196ce0ec6f1c65d2a9ad00215b2926ef2c416b8d97615265194477117  actions-runner-linux-x64-2.298.2.tar.gz" | shasum -a 256 -c
+# Extract the installer
+tar xzf ./actions-runner-linux-x64-2.298.2.tar.gz
+
+# Create the runner and start the configuration experience
+./config.sh --url https://github.com/havellaneda-ar/gh-test --token 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
+./run.sh
+
 EOF
 cd /home/ubuntu
 chmod +x user-data.sh
