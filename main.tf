9b6ed57690a129d879db61d12df03e30ae62bfa6
Hernan <47876073+havellaneda-ar@users.noreply.github.com>
2022-10-24 14:43:39 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 4.0.0"
+    }
+    random = {
+      version = ">= 2.1.2"
+    }
+  }
+
+  required_version = "~> 1.2.0"
+}
+
 provider "aws" {
   region = "us-east-1"
 }
