a3eed736bcf1d137beb2ab46aba742d69acdaf1c
Hernan <47876073+havellaneda-ar@users.noreply.github.com>
2022-10-27 14:27:25 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "github-runner-test" {
     vpc_security_group_ids = [aws_security_group.security_group.id]
     ami = "ami-08c40ec9ead489470"
     instance_type = "t2.micro"
-    user_data = templatefile("scripts/ec2.sh", {PERSONAL_ACCESS_TOKEN = GITHUB_TOKEN})
+    user_data = "${file("scripts/ec2.sh")}"
     tags= {Name = "github-runner", Type = "terraform"} 
 }
 
