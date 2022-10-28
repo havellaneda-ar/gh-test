ff56f9f0e58b3ecf70837127d45651b0361abd12
Hernan <47876073+havellaneda-ar@users.noreply.github.com>
2022-10-28 15:35:04 -0300

    Update ec2.sh

--- scripts/ec2.sh
+++ scripts/ec2.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 cat <<EOF >/home/ubuntu/user-data.sh
 #!/bin/bash
-# Create a folder 
-mkdir actions-runner && cd actions-runner# Download the latest runner package
-curl -o actions-runner-linux-x64-2.298.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.298.2/actions-runner-linux-x64-2.298.2.tar.gz
-# Validate the hash
-echo "0bfd792196ce0ec6f1c65d2a9ad00215b2926ef2c416b8d97615265194477117  actions-runner-linux-x64-2.298.2.tar.gz" | shasum -a 256 -c
-# Extract the installer
-tar xzf ./actions-runner-linux-x64-2.298.2.tar.gz
-
-# Create the runner and start the configuration experience
-
-curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: token ${personal_access_token} \
-  https://api.github.com/repos/havellaneda-ar/gh-test/actions/runners/registration-token > out.txt
-
-runner_token=\$(cat out.txt | grep -w "token" | cut -d'"' -f4)
-
-./config.sh --url https://github.com/havellaneda-ar/gh-test --token ${runner_token}
-./run.sh
-
+wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+chmod +x ./jq
+sudo cp jq /usr/bin
+curl --request POST 'https://api.github.com/repos/your_user/your_repository_name/actions/runners/registration-token' --header "Authorization: token ${personal_access_token}" > output.txt
+runner_token=\$(jq -r '.token' output.txt)
+mkdir ~/actions-runner
+cd ~/actions-runner
+curl -O -L https://github.com/actions/runner/releases/download/v2.263.0/actions-runner-linux-x64-2.263.0.tar.gz
+tar xzf ~/actions-runner/actions-runner-linux-x64-2.263.0.tar.gz
+rm ~/actions-runner/actions-runner-linux-x64-2.263.0.tar.gz
+~/actions-runner/config.sh --url https://github.com/your_user/your_repository_name/ --token \$runner_token --name "Github EC2 Runner" --unattended
+~/actions-runner/run.sh
 EOF
 cd /home/ubuntu
 chmod +x user-data.sh
