f586c4f24d3086fd838f1488bd3f383e5aac183f
Hernan <47876073+havellaneda-ar@users.noreply.github.com>
2022-10-29 17:56:23 -0300

    Update ec2.sh

--- scripts/ec2.sh
+++ scripts/ec2.sh
@@ -2,8 +2,6 @@
 cat <<EOF >/home/ubuntu/user-data.sh
 
 !/bin/bash
-# Create a folder 
-mkdir actions-runner && cd actions-runner
 
 # Download the latest runner package
 curl -o actions-runner-linux-x64-2.298.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.298.2/actions-runner-linux-x64-2.298.2.tar.gz
