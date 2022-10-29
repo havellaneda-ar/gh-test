f5112644471b056dad3706ca6242e15a9b3ae04f
Hernan <47876073+havellaneda-ar@users.noreply.github.com>
2022-10-29 17:11:31 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,14 +17,20 @@ curl -o actions-runner-linux-x64-2.298.2.tar.gz -L https://github.com/actions/ru
 echo "0bfd792196ce0ec6f1c65d2a9ad00215b2926ef2c416b8d97615265194477117  actions-runner-linux-x64-2.298.2.tar.gz" | shasum -a 256 -c
 # Extract the installer
 tar xzf ./actions-runner-linux-x64-2.298.2.tar.gz
+  
+# Download the personal code to connect github
+curl -o personal.token -L https://raw.githubusercontent.com/havellaneda-ar/gh-test/main/personal.code
+token=$(cat personal.token| base64 --decode)
+
+
 # Create the runner and start the configuration experience
-curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $github.token" \
+curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $token" \
   https://api.github.com/repos/havellaneda-ar/gh-test/actions/runners/registration-token > token_output.txt
-echo $GITHUB_TOKEN > tokens.txt
-echo $github.token >> tokens.txt
-echo "$GITHUB_TOKEN" >> tokens.txt
 
 token_runner=$(cat token_output.txt | grep -w "token" | cut -d'"' -f4)
+
+echo $token_runner
+
 ./config.sh --url https://github.com/havellaneda-ar/gh-test --token $token_runner
 ./run.sh
   
